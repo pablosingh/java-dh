a9d9f22a379a06075ac89737603f419fecc2dc1a
pablosingh <pablo22_4@hotmail.com>
2025-07-03 23:25:02 -0300

    Assertions - Parte 2

--- Test/01-intro-test/src/AssertionsTest.java
+++ Test/01-intro-test/src/AssertionsTest.java
@@ -1,8 +1,10 @@
 import org.junit.jupiter.api.Test;
 
-import java.util.Objects;
+import java.time.Duration;
+import java.util.*;
 import java.util.function.BooleanSupplier;
 
+import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AssertionsTest {
@@ -47,5 +49,72 @@ public class AssertionsTest {
     void notNullAssertion(){
         Object course = new Object();
         assertNotNull(course, "The course should be not null");
+    }
+
+    @Test
+    void nullAssertion(){
+        Object object = null;
+        assertNull(object, "Should be a null");
+    }
+    @Test
+    void sameAssertion(){
+        String course = "Backend";
+        Optional<String> optional = Optional.of(course);
+        assertSame("Backend", course);
+    }
+    @Test
+    void notSameAssertion(){
+        String course = "Backend";
+        String otherCourse = "POO";
+        Optional<String> optional = Optional.of(course);
+        assertNotSame(course, otherCourse);
+        assertNotSame(course, optional);
+    }
+
+    @Test
+    void allAssertions(){
+        assertAll(
+                ()-> assertEquals(2,2),
+                ()-> assertNotEquals(3 , 2*2),
+                ()-> assertNull(null, "Should be a null")
+        );
+    }
+    @Test
+    void iterableEqualsAssertion(){
+        Iterable<String> arrayList = new ArrayList<>(asList("Java", "Junit", "Backend"));
+        Iterable<String> linkedList = new LinkedList<>(asList("Java", "Junit", "Backend"));
+        assertIterableEquals(arrayList, linkedList);
+    }
+    @Test
+    void throwsAssertion1(){
+        Exception exception = assertThrows(NumberFormatException.class, ()-> {
+            Integer.parseInt("a");
+        });
+    }
+    @Test
+    void timeOutAssertion(){
+        assertTimeout(
+                Duration.ofSeconds(2),
+                ()-> {
+                    Thread.sleep(100);
+                }
+            );
+    }
+
+    @Test
+    void timeOutPreemptivelyAssertion(){
+        assertTimeoutPreemptively(
+                Duration.ofSeconds(2),
+                ()-> {
+                    Thread.sleep(10000);
+                }
+        );
+    }
+
+    @Test
+    void linesMatchAssertion(){
+        List<String> expected = asList("Java", "123", "Junit");
+        List<String> actual = asList("Java", "123", "Junit");
+        assertLinesMatch(expected, actual);
     }
 }
