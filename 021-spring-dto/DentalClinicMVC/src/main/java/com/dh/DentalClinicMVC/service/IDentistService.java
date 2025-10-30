e937ad15dd7a3dc65c68c8c8fc8e554a360f1662
pablosingh <pablo22_4@hotmail.com>
2025-10-29 22:49:00 -0300

    Practica de Spring con DTO

---
+++ 021-spring-dto/DentalClinicMVC/src/main/java/com/dh/DentalClinicMVC/service/IDentistService.java
@@ -0,0 +1,14 @@
+package com.dh.DentalClinicMVC.service;
+
+import com.dh.DentalClinicMVC.entity.Dentist;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IDentistService {
+    Dentist save (Dentist dentist);
+    Optional<Dentist> findById(Long id);
+    void update(Dentist dentist);
+    void delete(Long id);
+    List<Dentist> findAll();
+}
